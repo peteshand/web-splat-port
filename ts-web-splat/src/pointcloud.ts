df257ef39213dbc9a032c5eeb8f04cb400d2e621
Pete Shand <pete.j.shand@gmail.com>
2025-08-14 20:34:45 +1000

    feat(pointcloud): create GPU buffers for gaussians and SH coeffs

--- ts-web-splat/src/pointcloud.ts
+++ ts-web-splat/src/pointcloud.ts
@@ -5,15 +5,43 @@ export type Vec3 = [number, number, number];
 export interface Aabb<T = number> { min: [T, T, T]; max: [T, T, T]; }
 
 export class PointCloud {
-  constructor(public num_points: number, public sh_deg: number) {}
+  public gaussiansBuffer!: GPUBuffer;
+  public shCoefsBuffer!: GPUBuffer;
+  public num_points: number;
+  public sh_deg: number;
+
+  private constructor(num_points: number, sh_deg: number) {
+    this.num_points = num_points;
+    this.sh_deg = sh_deg;
+  }
 
   static async new(device: GPUDevice, pc: GenericGaussianPointCloud): Promise<PointCloud> {
-    // TODO: create buffers and bind groups
-    return new PointCloud(pc.num_points, pc.sh_deg);
+    const self = new PointCloud(pc.num_points, pc.sh_deg);
+    // Create storage buffers for gaussians and SH coefficients
+    self.gaussiansBuffer = createAndUploadBuffer(device, pc.gaussians, GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST);
+    self.shCoefsBuffer = createAndUploadBuffer(device, pc.sh_coefs, GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST);
+    return self;
   }
 
   compressed(): boolean { return false; }
   bbox(): Aabb<number> { return { min: [0,0,0], max: [0,0,0] }; }
   center(): Vec3 { return [0,0,0]; }
   up(): Vec3 | undefined { return undefined; }
+}
+
+function createAndUploadBuffer(device: GPUDevice, data: ArrayBuffer, usage: GPUBufferUsageFlags): GPUBuffer {
+  const buffer = device.createBuffer({
+    size: alignTo(data.byteLength, 4), // 4-byte alignment for safety
+    usage,
+    mappedAtCreation: true,
+  });
+  const dst = new Uint8Array(buffer.getMappedRange());
+  dst.set(new Uint8Array(data));
+  buffer.unmap();
+  return buffer;
+}
+
+function alignTo(n: number, alignment: number): number {
+  const r = n % alignment;
+  return r === 0 ? n : n + (alignment - r);
 }
